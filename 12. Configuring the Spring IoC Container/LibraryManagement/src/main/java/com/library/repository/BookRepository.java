4ca9a00f164f5a7831b93ef888bc082329353a6b
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-04 21:33:48 +0530

    Delete 12. Configuring the Spring IoC Container/LibraryManagement directory

--- 12. Configuring the Spring IoC Container/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void save(String bookName) {
-        // Logic to save the book in the repository
-        System.out.println("Saving book: " + bookName);
-    }
-}
