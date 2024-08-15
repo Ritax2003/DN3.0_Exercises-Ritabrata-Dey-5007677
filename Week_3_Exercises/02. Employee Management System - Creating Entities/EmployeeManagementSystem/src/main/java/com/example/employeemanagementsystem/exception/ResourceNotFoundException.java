8d058f95246501ee0f664cb8a7f452c8e8006513
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-15 09:41:26 +0530

    Delete Week_3_Exercises/02. Employee Management System - Creating Entities directory

--- Week_3_Exercises/02. Employee Management System - Creating Entities/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagementsystem.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Week_3_Exercises/02. Employee Management System - Creating Entities/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeemanagementsystem.repository;
-
-import com.example.employeemanagementsystem.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
