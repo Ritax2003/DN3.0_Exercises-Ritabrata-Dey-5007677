689b466a0a9fc7a78004faa776fbac743e516baa
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-15 09:41:16 +0530

    Delete Week_3_Exercises/01. Employee Management System - Overview and Setup/EmployeeManagementSystem directory

--- Week_3_Exercises/01. Employee Management System - Overview and Setup/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeemanagementsystem.repository;
-
-import com.example.employeemanagementsystem.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
